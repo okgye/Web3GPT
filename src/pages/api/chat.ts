6e6d067cd32134590d7fa0c0ace6735ba164ffb9
okgye <hongnoul@naver.com>
2025-02-16 05:44:30 +0000

    Add Hannah

--- src/pages/api/chat.ts
+++ src/pages/api/chat.ts
@@ -3,7 +3,8 @@ import axios from 'axios';
 
 const WHITELIST = [
   "0xDd3DEA716B95CC68b51f764bC25111C60De5D3F0".toLowerCase(),
-  "0xF3D8ED49c331B65Eca19cae99F7Dd090fE60BE22".toLowerCase() // ✅ Make sure your wallet is here
+  "0xF3D8ED49c331B65Eca19cae99F7Dd090fE60BE22".toLowerCase(),
+  "0x125a8f35ae11BA5ea94cAa70B404E1d1F785Ae45".toLowerCase() // ✅ Make sure your wallet is here
 ];
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
