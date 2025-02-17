049dca1fe30cff91142d09b3d685e3cc8bb02952
okgye <hongnoul@naver.com>
2025-02-17 13:07:33 +0000

    Add debugging wallet

--- src/pages/api/chat.ts
+++ src/pages/api/chat.ts
@@ -5,7 +5,8 @@ const WHITELIST = [
   "0xDd3DEA716B95CC68b51f764bC25111C60De5D3F0".toLowerCase(),
   "0xF3D8ED49c331B65Eca19cae99F7Dd090fE60BE22".toLowerCase(),
   "0x125a8f35ae11BA5ea94cAa70B404E1d1F785Ae45".toLowerCase(),
-  "0xE8e87d195451688384543AB10a7B3d6465136b2C".toLowerCase()
+  "0xE8e87d195451688384543AB10a7B3d6465136b2C".toLowerCase(),
+  "0x48DF98d9E769fC056e447839DD77F3745d914545".toLowerCase() // Debugging Wallet
 ];
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
